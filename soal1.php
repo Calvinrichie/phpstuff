38f87f6e8c2745d0f8fedd9a37361ac2d554f4e7
Calvinrichie <148748872+Calvinrichie@users.noreply.github.com>
2024-09-26 11:20:42 +0800

    Add files via upload

---
+++ soal1.php
@@ -0,0 +1,25 @@
+<?php
+'soal calvin';
+?>
+<!DOCTYPE html>
+<html>
+    <body>
+        
+    <?php 
+$number1 = [1,3,5,7,9,0,0,0,0];
+$number2 = [2,3,5,7,11];
+$m = 0;
+$n = count ($number1 [!0] OR $number2 [!0]);
+unset ($number1[0],$number2 [0]);
+$number3 = array_merge($number1,$number2);
+
+unset($number1[$m]);
+echo count ($number1);
+
+
+echo implode ($number3);
+
+?>
+</body>
+</html>
+
